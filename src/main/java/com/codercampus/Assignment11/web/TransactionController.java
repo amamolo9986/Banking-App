27861d7e6c491ed142a354a8ed9978d3880fa7c4
amamolo9986 <amamolo9986@yahoo.com>
2023-09-12 18:10:52 -0500

    Connected to database
    Transactions can be pulled up now

--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -1,13 +1,25 @@
 package com.codercampus.Assignment11.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.codercampus.Assignment11.domain.Transaction;
+import com.codercampus.Assignment11.service.TransactionService;
 
 @Controller
 public class TransactionController {
 	
+	@Autowired
+	private TransactionService transactionService;
+	
 	@GetMapping("/transactions")
-	public String getTransactions() {
+	public String getTransactions(ModelMap model) {
+		List<Transaction> transaction = transactionService.findAll();
+		model.put("transaction", transaction);
 		return "transactions";
 	}
 
